3d866c888c84bd9031be3abe3aa29a571242c2c6
Dave Bennett <david.a.bennett7@gmail.com>
2017-12-23 18:43:36 -0500

    Changed color to blue

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="theme-color" content="#343A40" />
+	<meta name="theme-color" content="#153B7C" />
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
@@ -34,7 +34,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content',
 		'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark" data-toggle="affix">
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top bg-blue" data-toggle="affix">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
